0709cda4ca86c8c2df678e772aec8b1f81bdfb72
nicoleamaral37 <123120925+nicoleamaral37@users.noreply.github.com>
2023-12-06 14:52:45 -0500

    Add files via upload

---
+++ Threat-Parse.ps1
@@ -0,0 +1,51 @@
+﻿$input = read-host "
+[I]PTables
+[W]indows
+Choose a ruleset"
+$drop_urls = @('https://rules.emergingthreats.net/blockrules/emerging-botcc.rules', 'https://rules.emergingthreats.net/blockrules/compromised-ips.txt')
+
+foreach ($u in $drop_urls)
+{
+    $temp = $u.split("/")
+    $fileName = $temp[-1]
+    
+    if( Test-Path $fileName)
+    {
+        continue
+    }
+    else
+    {
+        Invoke-WebRequest -Uri $u -OutFile $fileName
+    }
+}
+
+$inputPaths = @('.\compromised-ips.txt', '.\emerging-botcc.rules')
+
+$regexDrop = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+
+        Select-String -path $inputPaths -Pattern $regexDrop |`
+        ForEach-Object {$_.Matches } |`
+        ForEach-Object {$_.value } | Sort-Object | Get-Unique |
+        Out-File -FilePath "ips-bad.tmp"
+
+switch ($input)
+{
+    i 
+    {
+        (Get-Content -Path ".\ips-bad.tmp") | % `
+        {$_ -replace "^", "iptables -A INPUT -s " -replace "$", " -j DROP"} |`
+        Out-File -FilePath "iptables.bash"
+    }
+
+    w
+    {
+        (Get-Content -Path ".\ips-bad.tmp") | % `
+        {$_ -replace "^", "netsh advfirewall firewall add rule name= 'BLOCK IP ADDRESS - 
+        'dir=in action=block remoteip="} |`
+        Out-File -FilePath "windowsRuleset.bash"
+    }
+    
+}
+
+
+
